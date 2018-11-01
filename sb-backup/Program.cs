407cd1de2c0a7abf306b4f86b66d85c5028bcea1
Young-Jin Chung <youngjin.chung@hotmail.com>
2018-11-01 12:56:04 -0700

    List files

--- sb-backup/Program.cs
+++ sb-backup/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 
 namespace sb_backup
@@ -9,9 +10,22 @@ namespace sb_backup
         {
             while (true)
             {
-                Console.WriteLine("Hello World!");
-                Thread.Sleep(10000);
+                DoSomething();    
+                Thread.Sleep(100000);
             }
+        }
+
+        static void DoSomething()
+        {
+
+            DirectoryInfo dirInfo = new DirectoryInfo("/opt/app-root/src");
+
+            foreach(var file in dirInfo.GetFiles())
+            {
+                Console.WriteLine(file.Name);
+            }
+
+            Console.WriteLine(DateTime.Now);
         }
     }
 }
